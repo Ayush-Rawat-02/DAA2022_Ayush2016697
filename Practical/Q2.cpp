85f9b9e937e384103f6417a568238480d66f3633
Ayush <77936340+Ayush-Rawat-02@users.noreply.github.com>
2022-03-02 20:24:24 -0800

    Create Q2.cpp

---
+++ Practical/Q2.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n,t,key;
+  int arr[100];   //The array should be sorted in advance
+  cin>>t;
+  while(t){
+    cin>>n;
+    for(int i=0;i<n;i++) cin>>arr[i];
+    cin>>key;
+    int s=0,e=n-1;
+    int mid=s-(s-e)/2;
+    int comp=1;
+    while(s<=e){
+      if(arr[mid]==key)
+      {
+        cout<<"The key is present at index : "<<mid<<endl;
+        break;
+      }
+      else if(arr[mid]<key)
+      {
+        s=mid+1;
+        mid=s-(s-e)/2;
+      }
+      else
+      {
+        e=mid-1;
+        mid=s-(s-e)/2;
+      }
+      comp++;
+    }
+    cout<<"The total number of comparisions : "<<comp<<endl;
+    t--;
+  }
+  return 0;
+}
