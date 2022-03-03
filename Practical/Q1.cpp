0d7fc6e100aecb4846f65fb904a201b431d9e5d1
Ayush <77936340+Ayush-Rawat-02@users.noreply.github.com>
2022-03-02 20:13:58 -0800

    Update Q1.cpp

--- Practical/Q1.cpp
+++ Practical/Q1.cpp
@@ -1,14 +1,26 @@
 #include<iostream>
+#include<iostream>
 using namespace std;
 int main(){
-	int n,t;
+	int i,n,t,key;
 	int arr[100];
 	cin>>t;
 	while(t){
-	{
-		cin<<n;
-		for(int i=0;i<n;i++) cin>>arr[i];
-		cin<<key;
-	
+		cin>>n;
+		for(i=0;i<n;i++) cin>>arr[i];
+		cin>>key;
+		for(i=0;i<n;i++)
+			if(arr[i]==key){
+				cout<<"The key is present at index  : "<<i<<endl;
+				cout<<"Totoal number of comparisions : "<<i+1<<endl;
+				break;
+			}
+		if(i==n){
+		    cout<<"The key is not present"<<endl;
+		    cout<<"Total number of comparisions : "<<n<<endl;
+		    
+		}
 		t--;
 	}
+	return 0;
+}
