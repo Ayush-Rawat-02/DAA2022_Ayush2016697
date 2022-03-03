0affc547f7f5fa6068181a70f560d70aece5eb25
Ayush <77936340+Ayush-Rawat-02@users.noreply.github.com>
2022-03-02 20:02:32 -0800

    Created folders-"Theory" and "Practical"

---
+++ Practical/Q1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,t;
+	int arr[100];
+	cin>>t;
+	while(t){
+	{
+		cin<<n;
+		for(int i=0;i<n;i++) cin>>arr[i];
+		cin<<key;
+	
+		t--;
+	}
