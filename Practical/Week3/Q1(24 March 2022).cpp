e0c476cea376873136822276fa1763438a4a6a87
Ayush <77936340+Ayush-Rawat-02@users.noreply.github.com>
2022-03-24 09:27:34 +0530

    Update Q1(24 March 2022).cpp

--- Practical/Week3/Q1(24 March 2022).cpp
+++ Practical/Week3/Q1(24 March 2022).cpp
@@ -1,1 +1,34 @@
+/*
+Q1.WAP to implement bubble sort and find the total number of comparisions?
+*/
 
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter the number of elements in the array :";
+  cin>>n;
+  int arr[n];
+  cout<<"Enter the elements of the array one by one"<<endl;
+  
+  //Taking input from user
+  
+  for(int i=0;i<n;i++) cin>>arr[i];
+  
+  //Sorting the entered array
+  
+  for(int i=0;i<n-1;i++){
+    for(int j=i+1;j<n;j++){
+      if(arr[i]>arr[j]){
+        swap(arr[i],arr[j]);
+      }
+    }
+  }
+  
+  //Displaying the sorted array
+  
+  for(int i=0;i<n;i++)  cout<<arr[i]<<"  ";
+  cout<<endl;
+  return 0;
+}
