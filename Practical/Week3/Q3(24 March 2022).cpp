76b569cd4ef61e7de3eb7462dbdc8ee40c5e406d
Ayush <77936340+Ayush-Rawat-02@users.noreply.github.com>
2022-03-24 09:44:03 +0530

    Update Q3(24 March 2022).cpp

--- Practical/Week3/Q3(24 March 2022).cpp
+++ Practical/Week3/Q3(24 March 2022).cpp
@@ -1,1 +1,38 @@
 
+/*
+Q3.WAP to implement insertion sort and find the total number of comparisions?
+*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter the size of the array ";
+  cin>>n;
+  int arr[n];
+  cout<<"Enter the elements of the array one by one "<<endl;
+   
+  //Taking input from user
+  
+  for(int i=0;i<n;i++) cin>>arr[i];
+  
+  //Sorting the array
+  
+  int key=arr[0]
+    for(int i=1;i<n;i++)
+    {
+      int j=i;
+      while(arr[j]<key)
+      {
+        arr[j-1]=arr[j]
+      }
+      arr[j]=key;
+    }
+  
+  //Displaying the sorted array
+  
+  for(int i=0;i<n;i++) cout<<arr[i]<<"  ";
+  cout<<endl;
+  return 0;
+}
