dad2e56f926563786dddf68bd5bb38fb1c8ef29a
Ayush <77936340+Ayush-Rawat-02@users.noreply.github.com>
2022-03-24 10:10:53 +0530

    Update Q3(24 March 2022).cpp

--- Practical/Week3/Q3(24 March 2022).cpp
+++ Practical/Week3/Q3(24 March 2022).cpp
@@ -1,8 +1,3 @@
-
-/*
-Q3.WAP to implement insertion sort and find the total number of comparisions?
-*/
-
 #include<iostream>
 using namespace std;
 int main()
@@ -17,22 +12,27 @@ int main()
   
   for(int i=0;i<n;i++) cin>>arr[i];
   
-  //Sorting the array
+  //Sorting the array and counting the number of comparisons
   
-  int key=arr[0]
-    for(int i=1;i<n;i++)
-    {
-      int j=i;
-      while(arr[j]<key)
-      {
-        arr[j-1]=arr[j]
-      }
-      arr[j]=key;
+    int count=0;
+    for(int i=1;i<n;i++){
+        int key=arr[i];
+        int j=i-1;
+        while(j>=0&&arr[j]>key)
+        {
+            arr[j+1]=arr[j];
+            j--;
+            count++;
+        }
+        arr[j+1]=key;
+        cout<<"Loop no "<<i<<endl;
+        for(int i=0;i<n;i++) cout<<arr[i]<<"  ";
+        cout<<endl;
     }
   
-  //Displaying the sorted array
+  //Displaying the sorted array and the number of comparisons
   
   for(int i=0;i<n;i++) cout<<arr[i]<<"  ";
-  cout<<endl;
+  cout<<endl<<"Number of comparisions : "<<count<<endl;
   return 0;
 }
