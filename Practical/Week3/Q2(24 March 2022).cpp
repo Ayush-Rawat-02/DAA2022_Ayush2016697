3e21e7f5d1c2f0d7768fa09b2abdf2b88c074c24
Ayush <77936340+Ayush-Rawat-02@users.noreply.github.com>
2022-03-24 09:33:49 +0530

    Update Q2(24 March 2022).cpp

--- Practical/Week3/Q2(24 March 2022).cpp
+++ Practical/Week3/Q2(24 March 2022).cpp
@@ -1,1 +1,35 @@
+/*
+Q2.WAP to implement selection sort and find the total number of comparisions?
+*/
 
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter the number of elements in the array : ";
+  cin>>n;
+  int arr[n];
+  cout<<"Enter the elements of the array one by one"<<endl;
+  
+  //Taking input from user
+  
+  for(int i=0;i<n;i++) cin>>arr[i];
+  
+  //Sorting the array
+  
+  for(int i=0;i<n;i++){
+    int min=i;
+    for(int j=i+1;j<n;j++){
+      if(arr[j]<arr[min]) min=j;
+    }
+    swap(arr[i],arr[min]);
+  }
+  
+  //Displaying the array
+  
+  for(int i=0;i<n;i++) cout<<arr[i]<<"  ";
+  cout<<endl;
+  return 0;
+}
+      
